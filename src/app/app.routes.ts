c9a6ebbe6c27f41f4aa7320f860a21e4e9dc3654
justtife <farinubolu@gmail.com>
2025-05-04 00:45:10 +0300

    Project Setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,26 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: 'programs',
+    children: [
+      {
+        path: 'search',
+        loadComponent: () => import('./pages/program-search/program-search.page').then(m => m.ProgramSearchPage)
+      },
+      {
+        path: 'detail',
+        loadComponent: () => import('./pages/program-detail/program-detail.page').then(m => m.ProgramDetailPage)
+      },
+    ]
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'programs/search',
     pathMatch: 'full',
   },
+  {
+    path: '**',
+    redirectTo: 'programs/search',
+  },
+
 ];
---
+++ src/app/components/program-card/program-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-program-card',
+  templateUrl: './program-card.component.html',
+  styleUrls: ['./program-card.component.scss'],
+})
+export class ProgramCardComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
