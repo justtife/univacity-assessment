6e99363c58d8aacbf84c77e81b96ff1a8d7bfdae
justtife <farinubolu@gmail.com>
2025-05-06 14:43:01 +0300

    Added the filter modal component

--- src/app/components/filter-modal/filter-modal.component.ts
+++ src/app/components/filter-modal/filter-modal.component.ts
@@ -1,14 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import { IonicComponents } from '../ionic-components';
+import { ModalController } from "@ionic/angular/standalone"
+import SharedComponents from '../components';
+import { UtilityService } from 'src/app/services/utility.service';
+import { IndividualFilterComponent } from '../individual-filter/individual-filter.component';
 @Component({
   selector: 'app-filter-modal',
   templateUrl: './filter-modal.component.html',
   styleUrls: ['./filter-modal.component.scss'],
+  imports: [IonicComponents, SharedComponents]
 })
-export class FilterModalComponent  implements OnInit {
+export class FilterModalComponent implements OnInit {
+  tuitionRange = {
+    lower: 50000,
+    upper: 120000,
+  };
+  data = 1024;
+  selectedCountries = ['United States', 'United Kingdom'];
+  selectedDisciplines = ['Law', 'Mathematics'];
 
-  constructor() { }
-
-  ngOnInit() {}
-
+  constructor(private modalCtrl: ModalController, private uS: UtilityService) { }
+  ngOnInit() { }
+  closeFilter() {
+    this.modalCtrl.dismiss();
+  };
+  formatPinValue(value: number): string {
+    return `${value / 1000}k`
+  }
+  openIndividualFilter() {
+    this.uS.dialogOpener(IndividualFilterComponent, () => { }, null, null)
+  }
 }
