e8fb39ba2bc8078bac88f2038d70a599febc9a94
justtife <farinubolu@gmail.com>
2025-05-06 23:51:21 +0300

    App update

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,10 @@ import { provideEffects } from "@ngrx/effects"
 import { ProgramEffects } from './app/@ngrx/effects/program.effect';
 import { programReducer } from './app/@ngrx/reducers/program.reducer';
 import { addIcons } from 'ionicons';
-import { calendar, book, globe, time, shareOutline, heartOutline, ribbon, language, chevronUpOutline, chevronDownOutline, eyeOutline, trendingUpOutline, sparkles, informationCircleOutline, checkmarkCircleOutline, checkmarkDone, closeOutline, filterOutline, funnelOutline, chevronBack, chevronForward } from "ionicons/icons"
+import { calendar, book, globe, time, shareOutline, heartOutline, ribbon, language, chevronUpOutline, chevronDownOutline, eyeOutline, trendingUpOutline, sparkles, informationCircleOutline, checkmarkCircleOutline, checkmarkDone, closeOutline, filterOutline, funnelOutline, chevronBack, chevronForward, schoolOutline,timeOutline } from "ionicons/icons"
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
-addIcons({ calendar, time, book, globe, shareOutline, heartOutline, ribbon, language, chevronUpOutline, chevronDownOutline, eyeOutline, trendingUpOutline, sparkles, informationCircleOutline, checkmarkCircleOutline, checkmarkDone, closeOutline, filterOutline, funnelOutline, chevronBack, chevronForward });
+addIcons({ calendar, time, book, globe, shareOutline, heartOutline, ribbon, language, chevronUpOutline, chevronDownOutline, eyeOutline, trendingUpOutline, sparkles, informationCircleOutline, checkmarkCircleOutline, checkmarkDone, closeOutline, filterOutline, funnelOutline, chevronBack, chevronForward, schoolOutline,timeOutline });
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
