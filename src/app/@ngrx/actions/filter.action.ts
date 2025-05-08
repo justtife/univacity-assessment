bbbab1ac40f6cde6c13548843ad83aff6455fbde
justtife <farinubolu@gmail.com>
2025-05-08 22:19:44 +0300

    Included setTuitionRange action

--- src/app/@ngrx/actions/filter.action.ts
+++ src/app/@ngrx/actions/filter.action.ts
@@ -1,14 +1,25 @@
 import { createAction, props } from '@ngrx/store';
+import { OutputFormat } from 'src/app/models';
 export namespace FilterActions {
     export const setFilter = createAction(
         '[Filter] Set Filter',
-        props<{ filterType: string; ids: string[] }>()
+        props<{ filterType: string; ids: OutputFormat[] }>()
     );
-
+    export const removeFilterById = createAction(
+        '[Filter] Remove Filter By ID',
+        props<{ id: string }>()
+    );
     export const clearFilter = createAction(
         '[Filter] Clear Filter',
         props<{ filterType: string }>()
     );
-
+    export const setTuitionRange = createAction(
+        '[Filter] Set Tuition Range',
+        props<{ lower: number; upper: number; matchedPrograms?: OutputFormat[] }>()
+    );
+    export const setTuitionRangeActive = createAction(
+        '[Filter] Set Tuition Range Active',
+        props<{ active: boolean }>()
+    );
     export const clearAllFilters = createAction('[Filter] Clear All Filters');
 }
